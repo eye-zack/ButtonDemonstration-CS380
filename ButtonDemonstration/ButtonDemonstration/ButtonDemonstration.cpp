696dd75691963d7fe6447b3cf296084663558c96
eye-zack <isaacray649@gmail.com>
2023-10-22 16:59:48 -0500

    background color, text box
    changed the background color to black, as well as added a static text box above the buttons

--- ButtonDemonstration/ButtonDemonstration/ButtonDemonstration.cpp
+++ ButtonDemonstration/ButtonDemonstration/ButtonDemonstration.cpp
@@ -20,7 +20,7 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR cmdline, int cmd
     wc.hInstance = hInst;
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_PEN);
     wc.lpszMenuName = NULL;
     wc.lpszClassName = CLSNAME;
     wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
@@ -87,6 +87,19 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR cmdline, int cmd
         hInst,
         NULL);      // Pointer not needed.
 
+    HWND hwndExample = CreateWindow(
+        L"STATIC", 
+        L"Text Goes Here", 
+        WS_VISIBLE | WS_CHILD | SS_CENTER,  // will probably need SS_OWNERDRAW. the owner window recieves a WM_DRAWITEM whenever the control needs to be drawn 
+        440, 
+        50, 
+        500, 
+        200, 
+        hwnd, 
+        (HMENU)4,
+        hInst, 
+        NULL);
+
     ShowWindow(hwnd, cmdshow);
     UpdateWindow(hwnd);
     while (GetMessage(&msg, NULL, 0, 0)) {
@@ -101,7 +114,10 @@ LRESULT CALLBACK winproc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp)
         PostQuitMessage(0);
     } if (wm == WM_COMMAND) {
         if (HIWORD(wp) == BN_CLICKED && LOWORD(wp) == 1) {
-            MessageBox(hwnd, L"You clicked OK", L"BN_CLICKED", MB_ICONINFORMATION);
+            MessageBox(hwnd, L"You clicked ONE", L"BN_CLICKED", MB_ICONINFORMATION);
+        }
+        else if (HIWORD(wp) == BN_CLICKED && LOWORD(wp) == 2) {
+            MessageBox(hwnd, L"You clicked TWO", L"BN_CLICKED", MB_ICONINFORMATION);
         }
         else if (HIWORD(wp) == BN_CLICKED && LOWORD(wp) == 3) {
             PostQuitMessage(0);
