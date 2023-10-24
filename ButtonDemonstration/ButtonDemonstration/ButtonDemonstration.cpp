0c7551830f4c5f7faffb56a96a57284c9ab611cb
eye-zack <isaacray649@gmail.com>
2023-10-24 06:47:22 -0500

    Fixed buttons
    buttons now create the text box.
    still need to create/fix font for text

--- ButtonDemonstration/ButtonDemonstration/ButtonDemonstration.cpp
+++ ButtonDemonstration/ButtonDemonstration/ButtonDemonstration.cpp
@@ -87,18 +87,7 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR cmdline, int cmd
         hInst,
         NULL);      // Pointer not needed.
 
-    HWND hwndExample = CreateWindow(
-        L"STATIC", 
-        L"Text Goes Here", 
-        WS_VISIBLE | WS_CHILD | SS_CENTER,  // will probably need SS_OWNERDRAW. the owner window recieves a WM_DRAWITEM whenever the control needs to be drawn 
-        440, 
-        50, 
-        500, 
-        200, 
-        hwnd, 
-        (HMENU)4,
-        hInst, 
-        NULL);
+   
 
     ShowWindow(hwnd, cmdshow);
     UpdateWindow(hwnd);
@@ -115,13 +104,36 @@ LRESULT CALLBACK winproc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp)
             PostQuitMessage(0);
         case WM_COMMAND: 
             if (HIWORD(wp) == BN_CLICKED && LOWORD(wp) == 1) {
-                MessageBox(hwnd, L"You clicked ONE", L"BN_CLICKED", MB_ICONINFORMATION);
+                CreateWindow(
+                    L"STATIC",
+                    L"ONE",
+                    WS_VISIBLE | WS_CHILD | SS_CENTER,  // will probably need SS_OWNERDRAW. the owner window recieves a WM_DRAWITEM whenever the control needs to be drawn 
+                    440,
+                    50,
+                    500,
+                    200,
+                    hwnd,
+                    (HMENU)4,
+                    HINSTANCE(),
+                    NULL);
+                break;
             }
             else if (HIWORD(wp) == BN_CLICKED && LOWORD(wp) == 2) {
-                MessageBox(hwnd, L"You clicked TWO", L"BN_CLICKED", MB_ICONINFORMATION);
+                CreateWindow(
+                    L"STATIC",
+                    L"TWO",
+                    WS_VISIBLE | WS_CHILD | SS_CENTER,  // will probably need SS_OWNERDRAW. the owner window recieves a WM_DRAWITEM whenever the control needs to be drawn 
+                    440,
+                    50,
+                    500,
+                    200,
+                    hwnd,
+                    (HMENU)4,
+                    HINSTANCE(),
+                    NULL);
+                break;
             }
             else if (HIWORD(wp) == BN_CLICKED && LOWORD(wp) == 3) {
-                MessageBox(hwnd, L"You clicked QUIT", L"BN_CLICKED", MB_ICONINFORMATION);
                 PostQuitMessage(0);
             }
     }
